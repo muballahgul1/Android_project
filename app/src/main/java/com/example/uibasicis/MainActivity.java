41087fec69d71c866e7c56d74788f1a0276de854
Muballahgul <gul_lisha@yahoo.com>
2021-02-23 20:17:41 -0500

    I am commiting some more code

--- app/src/main/java/com/example/uibasicis/MainActivity.java
+++ app/src/main/java/com/example/uibasicis/MainActivity.java
@@ -4,17 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView txtHello;
+    private EditText editTxtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button helloBtnClick=findViewById(R.id.helloBtnClick);
+        helloBtnClick.setOnClickListener(this);
+         editTxtName=findViewById(R.id.ediTxtName);
+         txtHello=findViewById(R.id.txtHello);
     }
 
-    public void helloBtnClick(View view) {
-        TextView txtWelcome=findViewById(R.id.txtWelcome);
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.helloBtnClick:
+                Toast.makeText(this, "Hello button is clicked", Toast.LENGTH_SHORT).show();
+                txtHello.setText("Hello"+editTxtName.getText().toString());
+                break;
+            default:
+                break;
+        }
     }
 }
